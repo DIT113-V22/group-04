e33e8227241ac804149552017a2bc48b28c8d047
aieh <guseisai2student,gu.se>
2022-04-23 21:27:18 +0200

    Draw control screen, Speed bar issue #23

--- android/Drawer/app/src/main/java/com/example/drawer/DrawControl.java
+++ android/Drawer/app/src/main/java/com/example/drawer/DrawControl.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class DrawControl extends AppCompatActivity {
 
     private Button readMeScreen;
     private Button manualControlScreen;
     private Button drawControlScreen;
+    Button runBttn;
+    Button uploadBttn;
+    TextInputEditText text;
+    SeekBar seekBar;
+    TextView speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,28 @@ public class DrawControl extends AppCompatActivity {
         readMeScreen = findViewById(R.id.ReadMEScreen);
         manualControlScreen = findViewById(R.id.ManualScreen);
         drawControlScreen = findViewById(R.id.DrawScreen);
+        runBttn = findViewById(R.id.runButton);
+        uploadBttn = findViewById(R.id.uploadButton);
+        text = findViewById(R.id.textBox);
+        seekBar = findViewById(R.id.seekbar);
+        speed = findViewById(R.id.speed);
+
+
+        seekBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         readMeScreen.setOnClickListener(new View.OnClickListener() {
             @Override
