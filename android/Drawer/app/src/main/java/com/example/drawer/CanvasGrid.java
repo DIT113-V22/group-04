3f6df21066716dd6a5f1430f55018da6f18b7ffe
Kevin Collins <nivek049@gmail.com>
2022-05-19 13:42:02 +0200

    Modifies the movement algorithm to only consider cells drawn directly by the user Helps #28

--- android/Drawer/app/src/main/java/com/example/drawer/CanvasGrid.java
+++ android/Drawer/app/src/main/java/com/example/drawer/CanvasGrid.java
@@ -35,6 +35,8 @@ public class CanvasGrid extends View {
     private int cellLength = 50;
     private Paint blackPaint = new Paint();
     private boolean[][] cellChecked = new boolean[50][100];
+    private boolean[][] pureCellChecked = new boolean[50][100];
+    private VectorMap vectorMap = new VectorMap();
     int lastx;
     int lasty;
 
@@ -76,6 +78,7 @@ public class CanvasGrid extends View {
 
     public void clear(){
         cellChecked = new boolean[numColumns][numRows];
+        pureCellChecked = new boolean[numColumns][numRows];
         pointQueue.clear();
         invalidate();
     }
@@ -110,7 +113,98 @@ public class CanvasGrid extends View {
         }
 
         cellChecked = new boolean[numColumns][numRows];
+        pureCellChecked = new boolean[numColumns][numRows];
         invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (numColumns == 0 || numRows == 0) {
+            return;
+        }
+
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
+                if (cellChecked[i][j]) {
+                    canvas.drawRect(i * cellLength, j * cellLength,
+                            (i + 1) * cellLength, (j + 1) * cellLength,
+                            blackPaint);
+//                    if (!pointQueue.contains(new Point(i, j))) {
+//                        pointQueue.add(new Point(i, j));
+//                    }
+                }
+                if (pureCellChecked[i][j]) {
+                    if (!pointQueue.contains(new Point(i, j))) {
+                        pointQueue.add(new Point(i, j));
+                    }
+                }
+            }
+        }
+
+        // TODO Are getters needed here? Perhaps attributes should be used -KC
+
+        for (int i = 1; i < numColumns; i++) {
+            canvas.drawLine(i * cellLength, 0, i * cellLength, getNumRows() * cellLength, blackPaint);
+        }
+
+        for (int i = 1; i < numRows; i++) {
+            canvas.drawLine(0, i * cellLength, getNumColumns() * cellLength, i * cellLength, blackPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            int column = (int)(event.getX() / cellLength);
+            int row = (int)(event.getY() / cellLength);
+
+            try {
+
+                if (cellChecked[column][row] != true) {
+                    cellChecked[column][row] = true;
+                    pureCellChecked[column][row] = true;
+                    lastx = column;
+                    lasty = row;
+
+                }
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            invalidate();
+
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            int column = (int)(event.getX() / cellLength);
+            int row = (int)(event.getY() / cellLength);
+
+            try {
+
+                if (cellChecked[column][row] != true) {
+                    cellChecked[column][row] = true;
+                    pureCellChecked[column][row] = true;
+                    // if the difference between current x, y and new x, y is bigger than 1 draw a line in between
+                    if(Math.abs(row - lastx) > 1 || Math.abs(column - lasty) > 1 ) gridDrawLine(lastx, lasty, column, row);
+                    
+                    if(column != lastx && row != lasty){
+                        vectorMap.add(column, row);
+                    }
+                    lastx = column;
+                    lasty = row;
+                }
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            invalidate();
+        }
+        System.out.println("-----------Total------------: "+ vectorMap.calculateSize());
+        //System.out.println(vectorMap.toString());
+
+        return true;
     }
 
     // The current implementation assumes slow drawing (i.e. each cell will be adjacent
@@ -178,127 +272,38 @@ public class CanvasGrid extends View {
 
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        if (numColumns == 0 || numRows == 0) {
-            return;
-        }
-
-        for (int i = 0; i < numColumns; i++) {
-            for (int j = 0; j < numRows; j++) {
-                if (cellChecked[i][j]) {
-                    canvas.drawRect(i * cellLength, j * cellLength,
-                            (i + 1) * cellLength, (j + 1) * cellLength,
-                            blackPaint);
-                    if (!pointQueue.contains(new Point(i, j))) {
-                        pointQueue.add(new Point(i, j));
-                    }
-                }
-            }
-        }
-
-//        for (Point p : pointQueue) {
-//            Log.d("testa", String.valueOf(p));
-//        }
-
-
-        // TODO Are getters needed here? Perhaps attributes should be used -KC
-
-        for (int i = 1; i < numColumns; i++) {
-            canvas.drawLine(i * cellLength, 0, i * cellLength, getNumRows() * cellLength, blackPaint);
-        }
-
-        for (int i = 1; i < numRows; i++) {
-            canvas.drawLine(0, i * cellLength, getNumColumns() * cellLength, i * cellLength, blackPaint);
-        }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-            int column = (int)(event.getX() / cellLength);
-            int row = (int)(event.getY() / cellLength);
-
-            try {
-
-                if (cellChecked[column][row] != true) {
-                    cellChecked[column][row] = true;
-                    lastx = column;
-                    lasty = row;
-
-                }
-
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-
-            invalidate();
-
-        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-
-            int column = (int)(event.getX() / cellLength);
-            int row = (int)(event.getY() / cellLength);
-
-            try {
-
-                if (cellChecked[column][row] != true) {
-                    cellChecked[column][row] = true;
-                    // if the difference between current x, y and new x, y is bigger than 1 draw a line in between
-                    //if(Math.abs(row - lastx) > 1 || Math.abs(column - lasty) > 1 ) gridDrawLine(lastx, lasty, column, row);
-                    
-                    if(column != lastx && row != lasty){
-                        vectorMap.add(column, row);
-                    }
-                    lastx = column;
-                    lasty = row;
-                }
-
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-
-            invalidate();
-        }
-        System.out.println("-----------Total------------: "+ vectorMap.calculateSize());
-        //System.out.println(vectorMap.toString());
-
-        return true;
-    }
-
-
     //Bresenham's line algorithm for cell checked src: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
         // TODO: 2022-05-10 might need to rewrite the while true
 
-//    private void gridDrawLine(int x0, int y0, int x1, int y1){
-//        //Delta X, Y
-//        int dx = Math.abs(x1 - x0);
-//        int dy = -Math.abs(y1 - y0);
-//
-//        //incrementations variable using ternary operator
-//        int sx = (x0 < x1)? 1: -1;
-//        int sy = (y0 < y1)? 1: -1;
-//
-//        int error = dx + dy;
-//
-//        while (true){
-//            cellChecked[x0][y0] = true;
-//
-//            if(x0 == x1 && y0 == y1) break;
-//            int error2 = error + error; // 2* error
-//
-//            if (error2 >= dy){
-//                if(x0 == x1) break;
-//                error += dy;
-//                x0 += sx;
-//            }
-//            if (error2 <= dx){
-//                if(y0 == y1) break;
-//                error += dx;
-//                y0 += sy;
-//            }
-//        }
-//    }
+    private void gridDrawLine(int x0, int y0, int x1, int y1){
+        //Delta X, Y
+        int dx = Math.abs(x1 - x0);
+        int dy = -Math.abs(y1 - y0);
+
+        //incrementations variable using ternary operator
+        int sx = (x0 < x1)? 1: -1;
+        int sy = (y0 < y1)? 1: -1;
+
+        int error = dx + dy;
+
+        while (true){
+            cellChecked[x0][y0] = true;
+
+            if(x0 == x1 && y0 == y1) break;
+            int error2 = error + error; // 2* error
+
+            if (error2 >= dy){
+                if(x0 == x1) break;
+                error += dy;
+                x0 += sx;
+            }
+            if (error2 <= dx){
+                if(y0 == y1) break;
+                error += dx;
+                y0 += sy;
+            }
+        }
+    }
 
 
 }
