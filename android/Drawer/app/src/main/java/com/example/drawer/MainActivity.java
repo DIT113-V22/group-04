c85fbe28f4cb5b954e102131102a7b8b715c1230
Soarnir <soarnir@gmail.com>
2022-05-27 18:09:54 +0200

    Specify android device for tests
    trigger test actions
    Helps #12

--- android/Drawer/app/src/main/java/com/example/drawer/MainActivity.java
+++ android/Drawer/app/src/main/java/com/example/drawer/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         disBtn = findViewById(R.id.disBtn);
         disBtn.setOnClickListener(view -> mqttController.disconnect());
 
-        // Subscription button
+        // Subscription buttons
         subBtn = findViewById(R.id.subBtn);
         subBtn.setOnClickListener(view -> {
             mqttController.subscribe("/smartcar/report/startup");
