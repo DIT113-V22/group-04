d40b7b56a1fdb9e520c9f5c265e1a58f6c21a9d8
Soarnir <soarnir@gmail.com>
2022-05-27 19:16:20 +0200

    Alter artifact path to get all relevant log files
    Helps #12

--- android/Drawer/app/src/main/java/com/example/drawer/MainActivity.java
+++ android/Drawer/app/src/main/java/com/example/drawer/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         pubBtn.setOnClickListener(view ->
                 mqttController.publish("/smartcar/control/throttle", "50"));
 
+        // Disconnection button
         disBtn = findViewById(R.id.disBtn);
         disBtn.setOnClickListener(view -> mqttController.disconnect());
 
